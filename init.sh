613350889ad6e9a5269d34bc3b207c1b215076bc
Jonatan <jonatan@jonatanblue.se>
2016-01-24 22:03:45 +0000

    Init script for getting dependencies (until it is handled better)

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Get dependencies
+wget -q https://raw.githubusercontent.com/jonatanblue/comicannon/master/js/comicannon.js -O js/comicannon.js
+wget -q https://raw.githubusercontent.com/jonatanblue/comicannon/master/js/fabric.js -O js/fabric.js
